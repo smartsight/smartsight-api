8baa8772f67b3860636c3acc284864389d90d337
francoischalifour <francois.chalifour@gmail.com>
2017-02-18 19:49:56 +0100

    Stop the server if no Python path specified

--- config/index.js
+++ config/index.js
@@ -1,7 +1,17 @@
 const env = process.env.NODE_ENV || 'dev'
 
+if (!process.env.PYTHONPATH) {
+  console.error(
+`No Python interpreter specified
+Please set your default interpreter to Python 3:
+$ export PYTHONPATH=$(which python3)
+`)
+
+  process.exit(1)
+}
+
 module.exports = {
   host: process.env.SM_SERVER_HOST || env === 'dev' ? '0.0.0.0' : 'localhost',
   port: process.env.SM_SERVER_PORT || 3000,
-  pythonPath: process.env.PYTHONPATH || '/usr/bin/python3'
+  pythonPath: process.env.PYTHONPATH
 }
